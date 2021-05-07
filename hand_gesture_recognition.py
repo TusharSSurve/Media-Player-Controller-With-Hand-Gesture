896fa01fbd6dc051433e971ed1dc81ce4784b3c2
Tushar Surve <47970290+ttusharsurve567@users.noreply.github.com>
2021-05-07 21:24:53 +0530

    Add files via upload

--- hand_gesture_recognition.py
+++ hand_gesture_recognition.py
@@ -1,16 +1,30 @@
 import cv2 
 import mediapipe as mp
-from simple_gesture import simpleGesture
+from simple_gesture import simpleGesture,keyBinding,remap
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import numpy as np
 
 ############## Initialization ##############
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(cv2.CAP_DSHOW)
 
 mpHands = mp.solutions.hands 
 hands = mpHands.Hands(min_detection_confidence=0.80)
 mpDraw = mp.solutions.drawing_utils
 
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+volMin,volMax = volume.GetVolumeRange()[:2]
+
 lms = [4,8,12,16,20]
+count = 0
+sgList = []
+volBar = 400
 ############################################
+
 
 while True:
     success,img = cap.read()
@@ -26,21 +40,54 @@ while True:
                 
             mpDraw.draw_landmarks(img,handlandmark,mpHands.HAND_CONNECTIONS)
 
-    if lmList!=[]:
+    if lmList!=[]: 
         fingers = []
         if lmList[0][1] < lmList[8][1]:
             cv2.putText(img,'Hand Gesture Not Recognisable!!',(20,50),cv2.FONT_HERSHEY_COMPLEX,0.9,(0,255,0),2)
         else:
             if lmList[5][0] < lmList[17][0]:
-                fingers.append(True) if lmList[lms[0]][0] < lmList[lms[0]-2][0] else fingers.append(False)
+                fingers.append(True) if lmList[lms[0]][0] < lmList[lms[0]-1][0] else fingers.append(False)
             else:
-                fingers.append(True) if lmList[lms[0]][0] > lmList[lms[0]-2][0] else fingers.append(False)
+                fingers.append(True) if lmList[lms[0]][0] > lmList[lms[0]-1][0] else fingers.append(False)
 
             for lm in range(1,len(lms)):
                 fingers.append(True) if lmList[lms[lm]][1] < lmList[lms[lm]-2][1] else fingers.append(False)
-                
-            cv2.putText(img,simpleGesture(fingers,lmList[4],lmList[8]),(20,50),cv2.FONT_HERSHEY_COMPLEX,0.9,(0,255,0),2)
-        
+
+            sg = simpleGesture(fingers,lmList[4],lmList[8])
+            cv2.putText(img,sg,(20,50),cv2.FONT_HERSHEY_COMPLEX,0.9,(0,255,0),2)
+            if sg=='NONE':
+                pass
+            elif sg=='FOUR':
+                if lmList[9][0] < 320:
+                    vol = remap(lmList[9][1],360,90,volMin,volMax)
+                    volume.SetMasterVolumeLevel(vol, None)
+                    volBar = remap(lmList[9][1],360,90,400,150,1)
+            else:
+                if count<640:
+                    count+=32
+                    cv2.rectangle(img,(0,470),(count,480),(237,149,100),-1)
+                    cv2.putText(img,'Keep your hand position as it is for few seconds!',(100,440),cv2.FONT_HERSHEY_COMPLEX,0.5,(237,149,100),2)
+                    sgList.append(sg)
+                elif count==640:
+                    count = 0
+                    res = max(set(sgList), key = sgList.count)
+                    sgList = []
+                    if res=='INDEX':
+                        if lmList[8][0] < 320:
+                            keyBinding('LEFT')
+                        else:
+                            keyBinding('RIGHT')
+                    else:        
+                        keyBinding(res)
+    else:
+        count = 0
+        sgList = []
+
+    cv2.rectangle(img,(50,150),(85,400),(0,255,0),3)
+    cv2.rectangle(img,(50,int(volBar)),(85,400),(0,255,0),-1)
+
+    cv2.line(img,(320,0),(320,480),(237,149,100),1)
+    cv2.line(img,(0,240),(640,240),(237,149,100),1)        
     cv2.imshow('Image',img)
     if cv2.waitKey(1) & 0xff==ord('q'):
         break
--- simple_gesture.py
+++ simple_gesture.py
@@ -1,4 +1,6 @@
 from math import hypot
+import pyautogui as pag
+from scipy.interpolate import interp1d
 
 def getDistance(p1,p2):
     return hypot(p1[0] - p2[0],p1[1] - p2[1])
@@ -7,16 +9,31 @@ def isThumbNearIndexFinger(p1,p2):
     return getDistance(p1,p2) < 20
 
 def simpleGesture(fingers,p1,p2):
-    f_list = ['FIST!','ONE!','TWO!','THREE!','FOUR!','FIVE!'] 
-    if fingers[0]==False and fingers[1]==True and fingers[2]==False and fingers[3]==False and fingers[4]==True:
-        return 'ROCK!'
-    elif fingers[0]==True and fingers[1]==True and fingers[2]==False and fingers[3]==False and fingers[4]==True:
-        return 'SPIDERMAN!'
-    elif fingers[0]==False and fingers[1]==True and fingers[2]==True and fingers[3]==False and fingers[4]==False:
-        return 'PEACE!'
-    elif fingers[0]==True and fingers[1]==False and fingers[2]==False and fingers[3]==False and fingers[4]==True:
-        return 'HANG LOOSE!'
-    elif isThumbNearIndexFinger(p1,p2) and fingers[1]==False and fingers[2]==True and fingers[3]==True and fingers[4]==True:
-        return 'OK!'
+    if fingers.count(True)==5:
+        return 'FIVE'  # K Play
+    elif fingers.count(True)==0:
+        return 'FIST' # L Pause
+    elif fingers[0]==False and fingers[1]==True and fingers[2]==True and fingers[3]==True and fingers[4]==True:
+        return 'FOUR' # Volume 
+    elif fingers.count(True)==1 and fingers[0]==False and fingers[1]==True and fingers[2]==False and fingers[3]==False and fingers[4]==False:
+        return 'INDEX' # left & right arrow key
     else:
-        return f_list[fingers.count(True)]
+        return 'NONE'
+
+def keyBinding(res):
+    if res=='FIVE':
+        pag.press('k')
+    elif res=='FIST':
+        pag.press('l')
+    elif res=='RIGHT':
+        pag.press('right')
+    elif res=='LEFT':
+        pag.press('left')
+    
+def remap(x, in_min, in_max, out_min, out_max,flag=0):
+    if x>360:
+        return 400 if flag==1 else -63.5
+    if x<90:
+        return 150 if flag==1 else 0.0
+    m = interp1d([in_min,in_max],[out_min,out_max])
+    return float(m(x))
